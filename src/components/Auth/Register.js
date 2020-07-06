2edd6e7840f527e6d7b01aa6d2c3e284ba392a42
Autorooter <hadi.assalem@gmail.com>
2020-07-06 23:03:51 +0200

    add Login form with Firebase

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,6 +2,8 @@ import React from 'react';
 import firebase from '../../firebase';
 import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import md5 from 'md5';
+
 class Register extends React.Component {
 
     state = {
@@ -9,7 +11,9 @@ class Register extends React.Component {
         email: "",
         password: "",
         passwordConfirmation: "",
-        errors: []
+        errors: [],
+        loading: false,
+        usersRef: firebase.database().ref('users')
     };
 
     handleChange = event => {
@@ -51,39 +55,64 @@ class Register extends React.Component {
     }
 
     handleSubmit = event => {
+        event.preventDefault();
+
         if(this.isFormValid()){
-            event.preventDefault();
+            this.setState({errors: [], loading: true});
             // promise
             firebase.auth()
                     .createUserWithEmailAndPassword(
                         this.state.email, this.state.password
                     ).then(createdUser => {
                         console.log(createdUser);
-                    })
-                    .catch(err => {
-                        console.log(err);
+                        createdUser.user.updateProfile({
+                            displayName: this.state.username,
+                            photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+                        }).then(() => {
+                            this.saveUser(createdUser).then(() => {
+                                console.log('user saved');
+                            })
+                            this.setState({loading: false});
+                        }).catch(err => {
+                            console.error(err);
+                            this.setState({ errors: this.state.errors.concat(err), loading: false });
+                        });
+                    }).catch(err => {
+                        console.error(err);
+                        this.setState({errors: this.state.errors.concat(err), loading: false});
                     });
             }
     };
 
+    saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL
+        })
+    };
+
+    handleInputError = ( errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName))? "error" : ""
+    }
+
     render() {
-        const {username, email, password, passwordConfirmation, errors} = this.state;
+        const {username, email, password, passwordConfirmation, errors, loading} = this.state;
 
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
                     <Header as="h2" icon color="orange" textAlign="center">
-                         <Icon name="puzzle piece" color="orange"/>
+                         <Icon name="code branch" color="orange"/>
                          Register for DevChat
                     </Header>
                     <Form  onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
-                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" value={username} onChange= {this.handleChange} type="text"/>
-                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" value={email} onChange= {this.handleChange} type="email"/>
-                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" value={password} onChange= {this.handleChange} type="password"/>
-                            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation "value={passwordConfirmation}  onChange= {this.handleChange} type="password"/>
+                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" value={username} onChange= {this.handleChange} type="text" className={this.handleInputError(errors, 'username') } />
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" value={email} onChange= {this.handleChange} type="email" className={this.handleInputError(errors, 'email') }/>
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" value={password} onChange= {this.handleChange} type="password" className={this.handleInputError(errors, 'password') }/>
+                            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation "value={passwordConfirmation}  onChange= {this.handleChange} type="password" className={this.handleInputError(errors, 'password') }/>
 
-                            <Button color="orange" fluid size="large">Submit</Button>
+                            <Button disabled={loading} className={loading ? 'loading': ''} color="orange" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
                     {errors.length > 0 && (
