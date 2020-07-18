883e67c789ab4e0eeb8e5ca7aee4460615be0ebd
Autorooter <hadi.assalem@gmail.com>
2020-07-18 13:36:17 +0200

    create home page structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
+
+// stateless function copononet 
+
+const App = () => (
+  <Grid>
+    <ColorPanel />
+    <SidePanel />
+    <Messages />
+    <MetaPanel />
+  </Grid>
+)
 
 export default App;
